a0e3d5d0b942f92841375fd70ea570da31810c5b
Basmala Magdy Abdelaziz <basmalamagdy964@gmail.com>
2026-08-17 01:11:46 +0300

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,12 @@
+encoded_msg = "###!!@mocleW EPGTQ!!!6789"
+
+word1 = "mocleW"
+word2 = "EPGTQ"
+
+
+decoded_word1 = word1[::-1]  # بتبقى "Welcome"
+
+decoded_word2 = word2
+
+final_message = f"{decoded_word1} {decoded_word2}"
+print(final_message)
